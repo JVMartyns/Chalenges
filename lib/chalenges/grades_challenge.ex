52da3ae5f747cef9b579cf06e0b6033af736d0c1
JVMartyns <jvmartyns2015@gmail.com>
2022-10-06 19:54:29 -0300

    fix: change function "max" to "min"

--- lib/chalenges/grades_challenge.ex
+++ lib/chalenges/grades_challenge.ex
@@ -3,7 +3,8 @@ defmodule Chalenges.GradesChallenge do
   https://github.com/mathiassammer/challenges-jornada-em-ti/blob/master/challenge_1/desafio_das_notas.md
   """
   def run(list1, list2) do
-    max_length = max(length(list1), length(list2))
+    max_length = min(length(list1), length(list2))
+    IO.inspect(max_length)
 
     Enum.reduce(0..max_length, [0, 0], fn i, [x, y] ->
       cond do
